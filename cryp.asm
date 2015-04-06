8b9148c8954555fd44d0d641a49eb5b75bec092c
Jaken Herman <jch053@shsu.edu>
2015-04-05 22:33:50 -0500

    Update cryp.asm

--- cryp.asm
+++ cryp.asm
@@ -1,13 +1,41 @@
 	org	100h
 section .data
-msg	db	"We attack at dawn", '$'
 key	db	0Fh
+msg	db	"We attack at dawn", '$'
 
 section .bss
-encode	resb	20
-decode	resb	20
+encode	resb	80
+decode	resb	80
+buffer  resb    80
+CR      equ     13
+NULL    equ     0
 
 section .text
+;input a char from kbd into string1
+        mov     cx, 0
+        cld         		  ;process string left to right
+        mov     di, buffer       ;string1 is destination
+        mov     ah, 1             ;read char fcn
+        int     21h               ;read it
+while1:
+        cmp     al, CR            ;is char = CR?
+        je      endwhile1          ;if so, we're done
+        inc     cx
+        stosb                     ;store char just read into string1
+        int     21h               ;read next char
+        jmp     while1            ;loop until done
+endwhile1:
+        mov     byte [di], NULL      ;store ASCII null char
+
+
+        mov     dx, buffer        ;dx is the address arg to print
+        call    print             ;print string1
+
+print:
+        mov     si, dx            ;set up for lodsb
+        lodsb                     ;read first char into al
+        mov     ah, 2             ;display char fcn
+
 	mov	si, 0
 while:
 	cmp	byte [si+msg], '$'
