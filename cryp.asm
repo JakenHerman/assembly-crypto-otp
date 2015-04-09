06d59988d3bf015ad4f7d9089b550e08fde09293
Jaken Herman <jch053@shsu.edu>
2015-04-08 23:19:41 -0500

    prompt

--- cryp.asm
+++ cryp.asm
@@ -1,7 +1,8 @@
 	org	100h
 section .data
-key	db	0Fh
-msg	db	"We attack at dawn", '$'
+key					db				0Fh
+msg					db				"We attack at dawn", '$'
+prompt      db        "String to encrypt: ", CR, 10, '$'
 
 section .bss
 encode	resb	80
@@ -11,21 +12,26 @@ CR      equ     13
 NULL    equ     0
 
 section .text
+
+				mov			ah, 9							;print prompt
+				mov			dx, prompt				;what to print
+				int			21h								;print it.
+
 ;input a char from kbd into string1
         mov     cx, 0
-        cld         		  ;process string left to right
-        mov     di, buffer       ;string1 is destination
+        cld         		          ;process string left to right
+        mov     di, buffer        ;string1 is destination
         mov     ah, 1             ;read char fcn
         int     21h               ;read it
 while1:
         cmp     al, CR            ;is char = CR?
-        je      endwhile1          ;if so, we're done
+        je      endwhile1         ;if so, we're done
         inc     cx
         stosb                     ;store char just read into string1
         int     21h               ;read next char
         jmp     while1            ;loop until done
 endwhile1:
-        mov     byte [di], NULL      ;store ASCII null char
+        mov     byte [di], NULL   ;store ASCII null char
 
 
         mov     dx, buffer        ;dx is the address arg to print
@@ -37,6 +43,7 @@ print:
         mov     ah, 2             ;display char fcn
 
 	mov	si, 0
+
 while:
 	cmp	byte [si+msg], '$'
 	je	endwhile
@@ -45,12 +52,15 @@ while:
 	mov	[si+encode], al
 	inc	si
 	jmp	while
+
 endwhile:
 	mov	byte [si+encode], '$'
 	mov	ah, 9
 	mov	dx, encode
 	int	21h
 	mov	si, 0
+
+
 _while:
 	cmp	byte[si+encode], '$'
 	je	_endwhile
@@ -59,6 +69,7 @@ _while:
 	mov	[si+decode], al
 	inc	si
 	jmp	_while
+
 _endwhile:
 	mov	byte [si+decode], '$'
 	mov	ah, 2
@@ -73,4 +84,3 @@ _endwhile:
 	int	21h
 	mov	ax, 4C00h
 	int	21h
-	
