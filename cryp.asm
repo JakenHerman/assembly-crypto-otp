93d9066d32d2fb291a19cbf9f1d81715b03fad8f
Jaken Herman <jch053@shsu.edu>
2015-04-09 00:25:54 -0500

    annoyed

--- cryp.asm
+++ cryp.asm
@@ -1,59 +1,66 @@
 	org	100h
 section .data
-key					db				0Fh
-msg					db				"We attack at dawn", '$'
-prompt      db        "String to encrypt: ", CR, 10, '$'
+	bitmask				db				0Fh
+	msg					db				"We attack at dawn", '$'
+	prompt     			db        		"String to encrypt: ", CR, 10, '$'
 
 section .bss
-encode	resb	80
-decode	resb	80
-buffer  resb    80
-CR      equ     13
-NULL    equ     0
+	encode	resb	80
+	decode	resb	80
+	buffer  resb    80
+	CR      equ     13
+	NULL    equ     0
 
 section .text
 
-				mov			ah, 9							;print prompt
-				mov			dx, prompt				;what to print
-				int			21h								;print it.
+	mov			ah, 9					;print prompt
+	mov			dx, prompt				;what to print
+	int			21h						;print it.
 
 ;input a char from kbd into string1
-        mov     cx, 0
-        cld         		          ;process string left to right
-        mov     di, buffer        ;string1 is destination
-        mov     ah, 1             ;read char fcn
-        int     21h               ;read it
-while1:
+    mov     cx, 0
+    cld         		       ;process string left to right
+    mov     di, buffer        ;string1 is destination
+    mov     ah, 1             ;read char fcn
+    int     21h               ;read it
+
+INSTRING:
         cmp     al, CR            ;is char = CR?
-        je      endwhile1         ;if so, we're done
+        je      endInString         ;if so, we're done
         inc     cx
         stosb                     ;store char just read into string1
         int     21h               ;read next char
-        jmp     while1            ;loop until done
-endwhile1:
+        jmp     INSTRING            ;loop until done
+endInString:
         mov     byte [di], NULL   ;store ASCII null char
-
-
         mov     dx, buffer        ;dx is the address arg to print
         call    print             ;print string1
+        call    print2
 
 print:
-        mov     si, dx            ;set up for lodsb
+        mov     si, buffer            ;set up for lodsb
         lodsb                     ;read first char into al
         mov     ah, 2             ;display char fcn
+        int		21h
+        
+print2:
+        mov     si, buffer            ;set up for lodsb
+        lodsb                     ;read first char into al
+        mov     ah, 2             ;display char fcn
+        int		21h
 
 	mov	si, 0
-
-while:
+	
+CODER:
 	cmp	byte [si+msg], '$'
-	je	endwhile
+	je	ENDCODER
 	mov	al, [si+msg]
-	xor	al, [key]
+	xor	al, [bitmask]
 	mov	[si+encode], al
 	inc	si
-	jmp	while
+	jmp	CODER
 
-endwhile:
+ENDCODER:
 	mov	byte [si+encode], '$'
 	mov	ah, 9
 	mov	dx, encode
@@ -65,7 +72,7 @@ _while:
 	cmp	byte[si+encode], '$'
 	je	_endwhile
 	mov	al, [si+encode]
-	xor	al, [key]
+	xor	al, [bitmask]
 	mov	[si+decode], al
 	inc	si
 	jmp	_while
